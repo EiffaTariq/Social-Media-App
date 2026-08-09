1a69e66b3e27dbdbdf48cba7c3e1ec389871c91d
Eiffa Tariq <eiffatariq1@gmail.com>
2026-08-10 00:00:31 +0500

    2

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -4,20 +4,18 @@ import { deletePost, newPost, editCaption, getAllPosts, likeUnlikePost, commento
 import uploadFile from '../middleware/multer.js';
 
 const router = express.Router();
-//console.log("Upload type:", upload);
 
-router.post('/new', isAuth, uploadFile, newPost);
+// router.post('/new', isAuth, uploadFile, newPost);
 
-//router.post("/new", upload.single("file"), isAuth, newPost);
 router.put("/:id", isAuth, editCaption);
 router.delete('/:id', isAuth, deletePost);
 
 
 router.get("/all", isAuth, getAllPosts);
 
-router.post("/like/:id", isAuth, likeUnlikePost);
+// router.post("/like/:id", isAuth, likeUnlikePost);
 
-router.post("/comment/:id", isAuth, commentonPost);
-router.delete("/comment/:id", isAuth, deleteComment);
-router.get('/:id/timeline', getTimelinePosts);
+// router.post("/comment/:id", isAuth, commentonPost);
+// router.delete("/comment/:id", isAuth, deleteComment);
+// router.get('/:id/timeline', getTimelinePosts);
 export default router;
